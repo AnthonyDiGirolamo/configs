7f94212e24f06d1f064585cd5f9937bcb1e6a223
Anthony DiGirolamo <anthony.digirolamo@gmail.com>
2012-10-17 12:39:48 -0400

    moved oh-my-zsh customizations outside the forked submodule

---
+++ oh-my-zsh-custom/directories.zsh
@@ -0,0 +1,3 @@
+setopt auto_name_dirs
+setopt auto_pushd
+setopt pushd_ignore_dups
---
+++ oh-my-zsh-custom/plugins/git/git.plugin.zsh
@@ -0,0 +1,14 @@
+#
+# Will return the current branch name
+# Usage example: git pull origin $(current_branch)
+#
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
+}
