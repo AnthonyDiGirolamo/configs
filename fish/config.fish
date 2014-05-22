44e7202c541c6d8bd85c362d83b3bac84c5545a5
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-05-22 01:01:57 -0400

    added fish vi-mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
+. /Users/anthony/Preferences/fish/vi-mode.fish
+
+function fish_user_key_bindings
+  vi_mode_insert
+end
+
 function fish_prompt
-  printf '%s %s@%s %s⮀ %s %s⮀%s ' (set_color -b black) (whoami) (hostname|cut -d .  -f 1) (set_color -b blue black) (prompt_pwd) (set_color -b normal blue) (set_color normal)
+  printf '[%s]%s %s@%s %s⮀ %s %s⮀%s ' $vi_mode (set_color -b black) (whoami) (hostname|cut -d .  -f 1) (set_color -b blue black) (prompt_pwd) (set_color -b normal blue) (set_color normal)
 end
 
 function pd
@@ -13,4 +19,13 @@ end
 
 function o
   popd
+end
+
+function dl
+  for dir in $dirprev
+    echo $dir
+  end
+  for dir in $dirnext
+    echo $dir
+  end
 end
