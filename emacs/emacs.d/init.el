54e5bea0b568c3c97c9dbcf0b84c1273bc5cf1cc
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-05-27 14:03:50 -0400

    added key-chord, smart-mode-line, and rainbow-delimiters

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,30 +1,62 @@
-; GUI Font
+;; GUI Font
 (add-to-list 'default-frame-alist '(font . "PragmataPro-24" ))
+(menu-bar-mode 0)
+; (tool-bar-mode -1)
 
-(add-to-list 'load-path "~/.emacs.d/powerline")
-(require 'powerline)
-(powerline-evil-theme)
+;; Save Tempfiles in a temp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
-; Colors - launch emacs with TERM=xterm-16color for base16 and solarized
+;; no more typing out yes
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; smart-mode-line
+(add-to-list 'load-path "~/.emacs.d/dash") ; smart-mode-line prereq
+(require 'dash)
+(add-to-list 'load-path "~/.emacs.d/smart-mode-line")
+(setq sml/theme 'dark)
+(require 'smart-mode-line)
+(sml/setup)
+
+;; Powerline
+; (add-to-list 'load-path "~/.emacs.d/powerline")
+; (require 'powerline)
+; (powerline-evil-theme)
+
+;; Colorschemes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/base16-emacs.git")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized.git")
 ; (load-theme 'base16-eighties t)
 ; (load-theme 'base16-default t)
 ; (load-theme 'solarized-dark t)
+
+;; moe-theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/moe-theme")
 (add-to-list 'load-path "~/.emacs.d/moe-theme")
 (require 'moe-theme)
 (moe-dark)
-;(setq moe-theme-mode-line-color 'purple)
-;; (Available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
-;(powerline-moe-theme)
+;; moe-theme mode-lines (doesn't support evil)
+; (setq moe-theme-mode-line-color 'purple)
+; ;; (Available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
+; (powerline-moe-theme)
 
-; Packages
+;; MELPA Package Repository
 (require 'package)
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
 
-; SMEX - https://github.com/nonsequitur/smex
+(require 'rainbow-delimiters) 
+(global-rainbow-delimiters-mode)
+
+; ;; guide-key
+; (require 'guide-key)
+; (setq guide-key/guide-key-sequence '("C-x r" "C-x 4"))
+; (guide-key-mode 1)  ; Enable guide-key-mode
+
+;; SMEX - https://github.com/nonsequitur/smex
 (add-to-list 'load-path "~/.emacs.d/smex")
 (require 'smex)
 (smex-initialize)
@@ -32,7 +64,7 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ; old M-x
 
-; EVIL (emacs vi layer) http://www.emacswiki.org/emacs/Evil
+;; EVIL (emacs vi layer) http://www.emacswiki.org/emacs/Evil
 (add-to-list 'load-path "~/.emacs.d/undo-tree")
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
@@ -46,20 +78,45 @@
 
 (define-key evil-normal-state-map (kbd "SPC SPC") 'smex)
 
-; https://github.com/emacs-helm/helm
+;; key-chord
+;; http://www.emacswiki.org/emacs/key-chord.el
+(add-to-list 'load-path "~/.emacs.d/key-chord")
+(require 'key-chord)
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
+
+;; helm
+;; https://github.com/emacs-helm/helm
 (add-to-list 'load-path "~/.emacs.d/helm")
 (require 'helm-config)
 (global-set-key (kbd "C-c h") 'helm-mini)
 
+;; Markdown mode
 (add-to-list 'load-path "~/.emacs.d/markdown-mode")
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-; http://bling.github.io/blog/2013/10/27/emacs-as-my-leader-vim-survival-guide/
+;; defadvice
+;; http://bling.github.io/blog/2013/10/27/emacs-as-my-leader-vim-survival-guide/
 (defadvice evil-ex-search-next (after advice-for-evil-ex-search-next activate)
   (evil-scroll-line-to-center (line-number-at-pos)))
 
-(menu-bar-mode 0)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-rainbow-delimiters-mode t)
+ '(sml/active-background-color "color-238")
+ '(sml/inactive-background-color "color-235"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "white" :weight bold))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "red" :weight bold))))
+ '(sml/prefix ((t (:inherit sml/global :foreground "color-39")))))
