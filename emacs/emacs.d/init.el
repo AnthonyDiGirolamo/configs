0962785f0d80405ba40fc15e2979f17b0effbfdd
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-12-09 18:28:02 -0500

    updated emacs submodules, helm and init.el

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,3 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
+ '(sml/active-background-color "color-238")
+ '(sml/inactive-background-color "color-235"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "white" :weight bold))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "red" :weight bold))))
+ '(sml/prefix ((t (:inherit sml/global :foreground "color-39")))))
+
 ;; (setq inhibit-startup-message t)
 
 ;; (require 'mouse)
@@ -113,7 +130,7 @@
 ;; smart-mode-line
 (require 'smart-mode-line)
 (sml/setup)
-(sml/apply-theme 'dark)
+(sml/apply-theme 'light)
 
 ;; guide-key
 ;; (require 'guide-key)
@@ -153,8 +170,8 @@
 
 ;; Evil Addons
 (add-to-list 'load-path "~/.emacs.d/evil-surround")
-(require 'surround)
-(global-surround-mode 1)
+(require 'evil-surround)
+(global-evil-surround-mode 1)
 (add-hook 'web-mode-hook (lambda ()
                            (push '(?= . ("<%= " . " %>")) surround-pairs-alist)
                            (push '(?- . ("<% "  . " %>")) surround-pairs-alist)))
@@ -288,19 +305,3 @@
 ;; http://bling.github.io/blog/2013/10/27/emacs-as-my-leader-vim-survival-guide/
 (defadvice evil-ex-search-next (after advice-for-evil-ex-search-next activate)
   (evil-scroll-line-to-center (line-number-at-pos)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(sml/active-background-color "color-238")
- '(sml/inactive-background-color "color-235"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:foreground "white" :weight bold))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "red" :weight bold))))
- '(sml/prefix ((t (:inherit sml/global :foreground "color-39")))))
