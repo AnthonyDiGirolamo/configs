5e44bf8bdb9ca68a48eb5734e63a56ead5884ff7
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-06-22 13:16:18 -0400

    emacs: updated all packages, added evil via melpa and removed submodules, added ace-jump

--- emacs/emacs.d/elpa/deferred-20141208.1613/deferred-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "deferred" "20141208.1613" "Simple asynchronous functions for emacs lisp" (quote nil))
--- emacs/emacs.d/elpa/web-mode-20141209.851/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20141209.851" "major mode for editing html templates" (quote nil))
