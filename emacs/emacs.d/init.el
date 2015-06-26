b371863b9cd37c598e71088752856440c38381f1
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-06-26 00:16:01 -0400

    emacs: added flycheck and git-gutter

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -11,9 +11,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("b85fc9f122202c71b9884c5aff428eb81b99d25d619ee6fde7f3016e08515f07" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "1c57936ffb459ad3de4f2abbc39ef29bfb109eade28405fa72734df1bc252c13" "eafda598b275a9d68cc1fbe1689925f503cab719ee16be23b10a9f2cc5872069" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "7ad5c4ebefb34783a9cce63af92a25ebf76e06c47541124c5b3068379b6e1a49" "0ba316caf0af7785622970574caa8d3cf758a43c2d7c63299fc521814d9a64ac" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
+    ("de2c46ed1752b0d0423cde9b6401062b67a6a1300c068d5d7f67725adc6c3afb" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "b85fc9f122202c71b9884c5aff428eb81b99d25d619ee6fde7f3016e08515f07" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "1c57936ffb459ad3de4f2abbc39ef29bfb109eade28405fa72734df1bc252c13" "eafda598b275a9d68cc1fbe1689925f503cab719ee16be23b10a9f2cc5872069" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "7ad5c4ebefb34783a9cce63af92a25ebf76e06c47541124c5b3068379b6e1a49" "0ba316caf0af7785622970574caa8d3cf758a43c2d7c63299fc521814d9a64ac" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
  '(inhibit-startup-screen nil)
+ '(powerline-default-separator (quote contour))
  '(powerline-evil-tag-style (quote verbose))
+ '(powerline-height 25)
  '(sml/active-background-color "color-238")
  '(sml/inactive-background-color "color-235"))
 (custom-set-faces
@@ -22,6 +24,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ace-jump-face-foreground ((t (:background "color-18" :foreground "#ff8700" :weight bold))))
+ '(mode-line ((t (:background "gainsboro" :foreground "#073642" :inverse-video t :box nil :underline nil :slant normal :weight normal))))
+ '(mode-line-inactive ((t (:background "gainsboro" :foreground "#073642" :inverse-video t :box nil :underline nil :slant normal :weight normal))))
+ '(powerline-active1 ((t (:background "DodgerBlue1" :foreground " #073642"))))
+ '(powerline-active2 ((t (:background "grey40" :foreground " #073642"))))
+ '(powerline-inactive1 ((t (:background "gainsboro" :foreground "gray11"))))
+ '(powerline-inactive2 ((t (:background "gainsboro" :foreground "gray11"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "brightred" :weight bold))))
  '(rainbow-delimiters-depth-8-face ((t (:foreground "color-16" :weight bold))))
  '(sml/prefix ((t (:inherit sml/global :foreground "color-39")))))
@@ -121,10 +129,10 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized.git")
 ;; (load-theme 'base16-eighties t)
 ;; (load-theme 'base16-default t)
-; (load-theme 'solarized-dark t)
-; (load-theme 'wombat t)
-(load-theme 'leuven)
-(load-theme 'subatomic256)
+;; (load-theme 'solarized-dark t)
+;; (load-theme 'wombat t)
+;; (load-theme 'leuven)
+;; (load-theme 'subatomic256)
 
 ;; moe-theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/moe-theme")
@@ -332,3 +340,7 @@
 
 (require 'guide-key-tip)
 (setq guide-key-tip/enabled t)
+
+(global-git-gutter+-mode t)
+(
+(add-hook 'after-init-hook #'global-flycheck-mode)
