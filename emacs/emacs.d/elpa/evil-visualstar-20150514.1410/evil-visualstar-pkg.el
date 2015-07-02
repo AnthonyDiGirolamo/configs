f716dadc6dcd036ab200055b43c3697bb5de188b
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-07-02 08:38:31 -0400

    emacs: installed use-package and evil-visualstar

---
+++ emacs/emacs.d/elpa/evil-visualstar-20150514.1410/evil-visualstar-pkg.el
@@ -0,0 +1,1 @@
+(define-package "evil-visualstar" "20150514.1410" "Starts a * or # search from the visual selection" '((evil "0")) :url "https://github.com/bling/evil-visualstar" :keywords '("evil" "vim" "visualstar"))
