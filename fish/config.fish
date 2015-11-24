d4ee25404bca7eeb9ec91d26ced413d61fb7313f
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-11-24 13:05:29 -0500

    small updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,13 @@
-. /Users/anthony/Preferences/fish/vi-mode.fish
+# . /Users/anthony/Preferences/fish/vi-mode.fish
+# function fish_user_key_bindings
+#   vi_mode_insert
+# end
 
-function fish_user_key_bindings
-  vi_mode_insert
-end
+# function fish_prompt
+#   printf '[%s]%s %s@%s %s⮀ %s %s⮀%s %s ' $vi_mode (set_color -b black) (whoami) (hostname|cut -d .  -f 1) (set_color -b blue black) (prompt_pwd) (set_color -b normal blue) (set_color normal)
+# end
 
-function fish_prompt
-  printf '[%s]%s %s@%s %s⮀ %s %s⮀%s %s ' $vi_mode (set_color -b black) (whoami) (hostname|cut -d .  -f 1) (set_color -b blue black) (prompt_pwd) (set_color -b normal blue) (set_color normal)
-end
+fish_vi_mode
 
 function pd
   pushd $argv
