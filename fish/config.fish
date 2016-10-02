57e8a44b9bf34dc47cd77fe0c2653d0703812307
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-10-02 11:07:33 -0700

    fish update

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
-# function fish_prompt
-#   printf '[%s]%s %s@%s %s⮀ %s %s⮀%s %s ' $vi_mode (set_color -b black) (whoami) (hostname|cut -d .  -f 1) (set_color -b blue black) (prompt_pwd) (set_color -b normal blue) (set_color normal)
-# end
+function fish_prompt
+  # printf '[%s]%s %s@%s %s⮀ %s %s⮀%s %s ' $vi_mode (set_color -b black) (whoami) (hostname|cut -d .  -f 1) (set_color -b blue black) (prompt_pwd) (set_color -b normal blue) (set_color normal)
+  printf ' %s %s %s%s %s ' (set_color -b blue black) (prompt_pwd) (set_color -b normal blue) (set_color normal)
+# ~  Preferences   master 
+end
 
 fish_vi_key_bindings
 
