416aa5a89cecb984c937bb6a7aaeddc5382d12f0
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-02-22 14:34:03 -0800

    update takebreak.py

--- bin/takebreak.py
+++ bin/takebreak.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import signal
 import time
+# import pdb
+# pdb.set_trace()
 
-awaketime = int(float(sys.argv[1])*60)
-sleeptime = int(float(sys.argv[2])*60)
+awaketime = sys.argv[1]
+sleeptime = sys.argv[2]
 
-def take_a_break():
-    get = subprocess.check_output(["xrandr"]).decode("utf-8").split()
-    screens = [get[i-1] for i in range(len(get)) if get[i] == "connected"]
-    for scr in screens:
-        # uncomment either one of the commands below [1]
-        # darken the screen, or...
-        # subprocess.call(["xrandr", "--output", scr, "--brightness", "0"])
-        # turn it upside down :)
-        subprocess.call(["xrandr", "--output", scr, "--rotate", "inverted"])
-    time.sleep(sleeptime)
-    for scr in screens:
-        # back to "normal"
-        # subprocess.call(["xrandr", "--output", scr, "--brightness", "1"])
-        subprocess.call(["xrandr", "--output", scr, "--rotate", "normal"])
+get = subprocess.check_output(["xrandr"]).decode("utf-8").split()
+screens = [get[i-1] for i in range(len(get)) if get[i] == "connected"]
 
+def signal_handler(signal, frame):
+  print('Exiting')
+  reset_rotation()
+  sys.exit(0)
 
-while True:
-    time.sleep(awaketime)
-    take_a_break()
+signal.signal(signal.SIGINT, signal_handler)
+# print('Press Ctrl+C')
+# signal.pause()
+
+def invert_rotation():
+  for scr in screens:
+    # subprocess.call(["xrandr", "--output", scr, "--brightness", "0"])
+    subprocess.call(["xrandr", "--output", scr, "--rotate", "inverted"])
+
+def reset_rotation():
+  for scr in screens:
+    # subprocess.call(["xrandr", "--output", scr, "--brightness", "1"])
+    subprocess.call(["xrandr", "--output", scr, "--rotate", "normal"])
+
+def main():
+  while True:
+    print('Computer Time! Next break in {} minutes'.format(awaketime))
+    time.sleep(int(float(awaketime)*60))
+    invert_rotation()
+    print('Break Time! Computer time resumes in {} minutes'.format(sleeptime))
+    time.sleep(int(float(sleeptime)*60))
+    reset_rotation()
+
+if __name__ == '__main__':
+    main()
