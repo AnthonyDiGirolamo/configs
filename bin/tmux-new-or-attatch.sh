e73b647c9ca3029a5eabfdcbdc141b577404b83e
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-02-12 08:03:21 -0800

    update a few things

--- bin/tmux-new-or-attatch.sh
+++ bin/tmux-new-or-attatch.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
-tmux list-sessions && exec tmux at
-exec tmux
+which tmux && tmux list-sessions && exec tmux at
+which tmux && exec tmux
+which fish && exec fish
+which zsh && exec zsh
+exec bash
