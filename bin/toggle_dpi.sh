47ad5bac6346da7c0f4131790f5ffa9080b5f66a
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-02-16 10:39:03 -0800

    add 120dpi to toggle_dpi.sh

--- bin/toggle_dpi.sh
+++ bin/toggle_dpi.sh
@@ -7,6 +7,12 @@ CURRENT_DPI=$(xfconf-query -c xsettings -p /Xft/DPI)
 case "$CURRENT_DPI" in
     "96")
         echo Currently $CURRENT_DPI
+        echo Setting 120 DPI
+        xfconf-query -c xsettings -p /Xft/DPI -s 120
+        xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 32
+        ;;
+    "120")
+        echo Currently $CURRENT_DPI
         echo Setting 144 DPI
         xfconf-query -c xsettings -p /Xft/DPI -s 144
         xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 32
